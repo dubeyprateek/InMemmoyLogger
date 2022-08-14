ca0646ea4e74f2df211e6d32e41a40fbabb4f158
Prateek Kumar Dubey <pdubey@microsoft.com>
2022-08-14 17:20:08 +0800

    Added the JS extension file - No code yet.

---
+++ InMemmoyLogger/InMemoryLogger.js
@@ -0,0 +1,19 @@
+﻿function invokeScript()
+{
+    
+}
+
+function initializeScript()
+{
+
+}
+
+function GetLoggersList()
+{
+    host.diagnostics.debugLog("Function:: DoSomeThing \n");
+    let x = host.getModuleSymbol("InMemmoyLogger", "gLoggerInstanceList");
+    if (x == null)
+    {
+        throw new Error("gLoggerInstanceList not found\n");
+    }
+}
